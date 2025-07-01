726c5fc1d9eec61ecb63315e5c33eb5a208b9575
Piyush Raj <piyush.raj2024@lpu.in>
2025-07-01 17:52:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+    e.preventDefault(); // Prevent page reload
+    alert("Your message has been sent successfully!"); 
+});
+
+
+let a=14, b=16, res=a+b;
+console.log("Result is : " +res);
